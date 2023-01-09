e3f8d4c7e4c6236c45cdcf70531f2fbafaa1490c
martinezr12 <marcin.rogula1337@outlook.com>
2023-01-09 15:33:21 +0100

    zadania

--- JS/console.js
+++ JS/console.js
@@ -1,6 +1,7 @@
+//console
 console.log("Witaj świecie");
 console.log(window);
-console.log("Witaj",window,100);
+console.log("Witaj", window, 100);
 console.table([1, 2, 3, 4, 5]);
 console.group("grupa1");
 console.log("Ala ma kota");
@@ -11,14 +12,16 @@ console.log("Ala ma kota");
 console.log("Kot ma Alę");
 console.groupEnd();
 console.time("test 1");
-for (let i = 0; i < 100000; i++){
+for (let i = 0; i < 100000; i++) {
     ;
 }
 console.timeEnd("test 1")
-function test(){
+
+function test() {
     let i = 0;
     //debugger;//przerywa w tym miejscu działanie kodu
 }
+
 test();
 //zmienne
 var text = "ala";
@@ -31,8 +34,9 @@ const b = 0;
 console.log(b);
 const name = "Ala";
 
-const tab = [1,2,3];
+const tab = [1, 2, 3];
 tab[3] = 4;
 tab.push(5, 6);
 console.log(tab);
 //tab = [1, 2, 3, 4, 5, 6]; nie możemy zmienić wartości istniejącej tablicu
+//DOM
---
+++ JS/zad1.js
@@ -0,0 +1,9 @@
+//1
+const zmiana = document.querySelector("header");
+console.log({zmiana});
+zmiana.style.backgroundColor="yellow";
+//2
+const zmiana1 = document.querySelector("header h1");
+console.log(zmiana1);
+zmiana1.style.fontStyle="Arial Black";
+zmiana1.innerHTML= "Preambuła";
