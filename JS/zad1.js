e24a426b5d82bcea380bda7120cd260d9f333aa8
martinezr12 <marcin.rogula1337@outlook.com>
2023-01-13 10:26:29 +0100

    das

--- JS/zad1.js
+++ JS/zad1.js
@@ -5,5 +5,26 @@ zmiana.style.backgroundColor="yellow";
 //2
 const zmiana1 = document.querySelector("header h1");
 console.log(zmiana1);
-zmiana1.style.fontStyle="Arial Black";
+zmiana1.style.fontFamily="Arial Black";
 zmiana1.innerHTML= "Preambuła";
+//3
+const zmiana3 = document.querySelectorAll("section");
+console.log(zmiana3);
+for(const section of zmiana3) {
+    section.style.border = "2px solid brown";
+}
+//4
+const zmiana4 = document.querySelector("section.main_column h1");
+console.log(zmiana4);
+zmiana4.innerHTML="Specjalność szefa:";
+//5
+const zmiana5 = document.createElement("p");
+zmiana5.innerHTML="kebab na frytkach _-_";
+console.log(zmiana5);
+const zmiana51 = document.querySelector(".main_column");
+console.log(zmiana51);
+zmiana51.appendChild(zmiana5);
+//6
+const zmiana6 = document.querySelectorAll("section.side_column h1");
+console.log(zmiana6);
+const zmiana61 = ()
