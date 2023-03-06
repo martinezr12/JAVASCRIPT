60d7ada389d4824f0fb274815c456950ac44539e
martinezr12 <marcin.rogula1337@outlook.com>
2023-03-06 15:16:13 +0100

    essssssssssssssssssssssssssa

---
+++ JS/0303.js
@@ -0,0 +1,71 @@
+document.querySelector("#span1").addEventListener("click", function () {
+    const pree1 = document.querySelector("#pre1");
+    const spann1 = document.querySelector("#span1");
+    if (pree1.style.display==='none') {
+        spann1.innerHTML='&#8613;';
+        pree1.style.display='block';
+    }
+    else {
+        spann1.innerHTML='&#8615';
+        pree1.style.display='none';
+    }
+});
+document.querySelector("#span2").addEventListener("click", function () {
+    const pree2 = document.querySelector("#pre2");
+    const spann2 = document.querySelector("#span2");
+    if (pree2.style.display==='none') {
+        spann2.innerHTML='&#8613;';
+        pree2.style.display='block';
+    }
+    else {
+        spann2.innerHTML='&#8615';
+        pree2.style.display='none';
+    }
+});
+document.querySelector("#span3").addEventListener("click", function () {
+    const pree3 = document.querySelector("#pre3");
+    const spann3 = document.querySelector("#span3");
+    if (pree3.style.display==='none') {
+        spann3.innerHTML='&#8613;';
+        pree3.style.display='block';
+    }
+    else {
+        spann3.innerHTML='&#8615';
+        pree3.style.display='none';
+    }
+});document.querySelector("#span4").addEventListener("click", function () {
+    const pree4 = document.querySelector("#pre4");
+    const spann4 = document.querySelector("#span4");
+    if (pree4.style.display==='none') {
+        spann4.innerHTML='&#8613;';
+        pree4.style.display='block';
+    }
+    else {
+        spann4.innerHTML='&#8615';
+        pree4.style.display='none';
+    }
+});
+document.querySelector("#span5").addEventListener("click", function () {
+    const pree5 = document.querySelector("#pre5");
+    const spann5 = document.querySelector("#span5");
+    if (pree5.style.display==='none') {
+        spann5.innerHTML='&#8613;';
+        pree5.style.display='block';
+    }
+    else {
+        spann5.innerHTML='&#8615';
+        pree5.style.display='none';
+    }
+});
+document.querySelector("#span6").addEventListener("click", function () {
+    const pree6 = document.querySelector("#pre6");
+    const spann6 = document.querySelector("#span6");
+    if (pree6.style.display==='none') {
+        spann6.innerHTML='&#8613;';
+        pree6.style.display='block';
+    }
+    else {
+        spann6.innerHTML='&#8615';
+        pree6.style.display='none';
+    }
+});
