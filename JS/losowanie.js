c25f7a83dc6ed777b9c979b368bf97a74d42a0ea
Marcin <marcin.rogula1337@outlook.com>
2023-04-21 01:09:58 +0200

    easdad

---
+++ JS/losowanie.js
@@ -0,0 +1,31 @@
+const szukanieprzycisku = document.getElementById("przycisk");
+const szukaniewyniku = document.getElementById("wynik");
+const szukaniesprawdzania = document.getElementById("sprawdzanie");
+const parzysta = document.getElementById("parzysta");
+const dziesiatki = document.getElementById("dziesiatki");
+szukanieprzycisku.addEventListener('click', function (){
+    const min = 1;
+    const max = 30;
+    const liczba = Math.floor(Math.random() * (max - min+1)) +min;
+    szukaniewyniku.textContent = liczba
+    console.log(liczba);
+    if(liczba>=20){
+        szukaniesprawdzania.innerText='Wylosowana liczba jest większa lub równa 20'
+    }else {
+        szukaniesprawdzania.innerText=''
+    }
+    if(liczba % 2 ===0){
+        parzysta.innerText='Wylosowana liczba jest parzysta'
+    }else {
+        parzysta.innerText='Wylosowana liczba jest nieparzysta'
+    }
+    if(liczba<=9){
+        dziesiatki.innerText='Wylosowana liczba jest w pierwszej dziesiątce'
+    }else if(liczba<=19){
+        dziesiatki.innerText='Wylosowana liczba jest w drugiej dziesiątce'
+    }else if(liczba<=29){
+        dziesiatki.innerText='Wylosowana liczba jest w trzeciej dziesiątce'
+    }else {
+        dziesiatki.innerText='Wylosowana liczba jest w czwartej dziesiątce'
+    }
+});
