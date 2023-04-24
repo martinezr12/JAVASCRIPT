896d957c35a9097a6bc5a82738bf909a1225271f
martinezr12 <marcin.rogula1337@outlook.com>
2023-04-24 15:32:49 +0200

    funkcje

---
+++ JS/funkcje matematyczne.js
@@ -0,0 +1,9 @@
+const liczba1 = document.getElementById("liczba1");
+const liczba2 = document.getElementById("liczba2");
+// abs
+const szukanieabs = document.getElementById("pokazywanieabs");
+const szukanieabs1 = document.getElementById("pokazywanieabs1");
+const abs = Math.abs(-6.5);
+const abs1 = Math.abs(4.3);
+szukanieabs.textContent=abs;
+szukanieabs1.textContent=abs1;
